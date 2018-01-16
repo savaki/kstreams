501401b759b1a0e560e382ebd73a5fca44d4b5af
savaki <matt.ho@gmail.com>
2018-01-16 13:22:50 -0800

    updated license header

--- statestore.go
+++ statestore.go
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
+
 package kstreams
 
 // StateStore provides a storage engine for managing state maintained by a stream processor.
